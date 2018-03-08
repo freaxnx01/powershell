984598e4b3177e5d5d0bb0a11f5df4813af873da
freaxnx01 <35143611+freaxnx01@users.noreply.github.com>
2018-03-08 16:25:24 +0100

    Create functions.psm1

---
+++ mymodules/functions.psm1
@@ -0,0 +1,16 @@
+function Get-ProfileDirectoryPath
+{
+	return Split-Path $profile
+}
+
+function New-TemporaryDirectory
+{
+	$parent = [System.IO.Path]::GetTempPath()
+	[string] $name = [System.Guid]::NewGuid()
+	New-Item -ItemType Directory -Path (Join-Path $parent $name)
+}
+
+function Get-Guid
+{
+	return [System.Guid]::NewGuid()
+}

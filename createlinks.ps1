3cee8af7343f9e27ff2bce1db7ab77d4888ac4e2
Gitea <gitea@fake.local>
2018-03-09 10:20:02 +0100

    createlinks.ps1

---
+++ createlinks.ps1
@@ -0,0 +1,26 @@
+$MyInvocation
+
+# Profile
+$fileName = "Microsoft.PowerShell_profile.ps1"
+$target = "$env:SystemRoot\System32\WindowsPowerShell\v1.0\$fileName"
+if ((Test-Path $target) -eq $false) {
+	New-Item -ItemType SymbolicLink -Path $target -Value $fileName
+}
+
+# Profile VSCode
+$fileName = "Microsoft.PowerShell_profile.ps1"
+$targetFileName = "Microsoft.VSCode_profile.ps1"
+$target = "$env:USERPROFILE\Documents\WindowsPowerShell\$targetFileName"
+if ((Test-Path $target) -eq $false) {
+	New-Item -ItemType SymbolicLink -Path $target -Value $fileName
+}
+
+# mymodules
+$directoryName = "mymodules"
+$target = "$env:USERPROFILE\Documents\WindowsPowerShell\$directoryName"
+if ((Test-Path $target) -eq $false) {
+	New-Item -ItemType SymbolicLink -Path $target -Value $directoryName
+}
+
+Write-Host "Press any key to continue..."
+[void][System.Console]::ReadKey($true)
--- mymodules/functions.psm1
+++ mymodules/functions.psm1
@@ -14,3 +14,16 @@ function Get-Guid
 {
 	return [System.Guid]::NewGuid()
 }
+
+function Install-OpenHereAsAdministrator
+{
+	$menu = 'Open Windows PowerShell Here as Administrator'
+	$command = "$PSHOME\powershell.exe -NoExit -Command ""Set-Location '%V'"""
+
+	'directory', 'directory\background', 'drive' | ForEach-Object {
+		New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell" -Name runas\command -Force |
+		Set-ItemProperty -Name '(default)' -Value $command -PassThru |
+		Set-ItemProperty -Path {$_.PSParentPath} -Name '(default)' -Value $menu -PassThru |
+		Set-ItemProperty -Name HasLUAShield -Value ''
+	}
+}
